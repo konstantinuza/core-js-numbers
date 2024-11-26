8a99f253fc5f5f35b6ff11ca18195b5396c5093a
konstantinuza <kostya2.7phoenix@gmail.com>
2024-11-26 06:12:04 +0300

    feat: completed 44/56

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -480,8 +480,8 @@ function getFloatOnString(str) {
  * '1.234', 2           => 1
  * '10', 8              => 8
  */
-function getIntegerOnString(/* str, base */) {
-  throw new Error('Not implemented');
+function getIntegerOnString(str, base) {
+  return Number.parseInt(str, base);
 }
 
 /**
@@ -509,8 +509,8 @@ function isSafeInteger(number) {
  * 5.9  => 5
  * -5.1 => -6
  */
-function roundToSmallestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToSmallestInteger(number) {
+  return Math.floor(number);
 }
 
 /**
